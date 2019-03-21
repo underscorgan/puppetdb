1a8026347ccb90227b52f1692b34b7a489e4eced
Morgan Rhodes <morgan@puppet.com>
2019-03-21 13:42:21 -0700

    (maint) consul agent spike

--- docker/puppetdb/docker-entrypoint.d/30-consul.sh
+++ docker/puppetdb/docker-entrypoint.d/30-consul.sh
@@ -1,28 +1,5 @@
 #!/bin/sh
 
 if [ "$CONSUL_ENABLED" = "true" ]; then
-  ipaddress="$(ifconfig eth0 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-  CONSUL_HOSTNAME="${CONSUL_HOSTNAME:-consul}"
-  CONSUL_PORT="${CONSUL_PORT:-8500}"
-
-  cat <<SERVICEDEF > /puppet-service.json
-{
-  "name": "puppetdb",
-  "id": "$HOSTNAME",
-  "port": 8080,
-  "address": "$ipaddress",
-  "checks": [
-    {
-      "http": "http://$HOSTNAME:8080/status/v1/services/puppetdb-status",
-      "interval": "30s",
-      "deregister_critical_service_after": "10m"
-    }
-  ]
-}
-SERVICEDEF
-
-  curl \
-    --request PUT \
-    --data @puppet-service.json \
-    http://$CONSUL_HOSTNAME:$CONSUL_PORT/v1/agent/service/register
+  consul agent -config-dir=/etc/consul.d -retry-join "$CONSUL_HOSTNAME" &
 fi
--- docker/puppetdb/docker-entrypoint.sh
+++ docker/puppetdb/docker-entrypoint.sh
@@ -2,12 +2,28 @@
 
 set -e
 
+on_exit() {
+  # Propagate up the exit code of our container application
+  status=$?
+  # Perform cleanup/whatever on exit
+  echo "Shutting down!"
+  # Don't trigger this again when we exit below
+  trap '' EXIT TERM
+  if [ "$CONSUL_ENABLED" = "true" ]; then
+    pkill consul
+  fi
+  exit $status
+
+}
+
 for f in /docker-entrypoint.d/*.sh; do
     echo "Running $f"
     chmod +x "$f"
     "$f"
 done
 
-exec java $PUPPETDB_JAVA_ARGS -cp /puppetdb.jar \
+trap on_exit INT TERM EXIT
+
+su-exec root java $PUPPETDB_JAVA_ARGS -cp /puppetdb.jar \
     clojure.main -m puppetlabs.puppetdb.core "$@" \
     -c /etc/puppetlabs/puppetdb/conf.d/
